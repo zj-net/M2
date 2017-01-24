72aaab9cb32d8b427e8526804d83b95529a3a274
Tristen Allen <spyboticsguy@gmail.com>
2017-01-24 14:35:52 -0500

    Add functionality to Person5.calc().
    Method now performs string rotation as specified.

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  String prefix = input.substring(0,1);
+	  String suffix = input.substring(2);
+	  return suffix + prefix;
 	}
 	
 	/**
